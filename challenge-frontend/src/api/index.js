0270100f2cf84301866e3c6b14718b78436bb8fc
carlosvhz <carlosvhz@outlook.com>
2021-11-25 10:36:45 -0600

    details

--- challenge-frontend/src/api/index.js
+++ challenge-frontend/src/api/index.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_HTTPREQUEST
+      : "http://localhost:5000",
+});
 
 // User endpoints
 export const createUser = (data) => API.post("/user/createUser", data);
